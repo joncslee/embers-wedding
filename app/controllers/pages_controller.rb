65e6cbbfdc53a4e95112ade349d4caee602f8068
Jonathan Lee <joncslee@gmail.com>
2013-07-31 23:32:52 -0400

    initial layout work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,21 @@
 class PagesController < ApplicationController
   def landing
+    render :layout => 'landing'
   end
+
+  def about_us
+  end
+
+  def bridal_party
+  end
+
+  def wedding_details
+  end
+
+  def music
+  end
+
+  def registry
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ EmbersWedding::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'pages#landing'
+  # root 'pages#landing'
+  root 'pages#about_us'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get 'about-us' => 'pages#about_us'
+  get 'bridal-party' => 'pages#bridal_party'
+  get 'wedding-details' => 'pages#wedding_details'
+  get 'registry' => 'pages#registry'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
